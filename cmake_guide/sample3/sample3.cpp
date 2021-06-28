d72969816dffcf9c238d3a0ffd5931841ae74db1
leichen <1327712813@qq.com>
2021-06-28 22:21:08 +0800

    Add CMake dynamic library sample.

---
+++ cmake_guide/sample3/sample3.cpp
@@ -0,0 +1,12 @@
+#include "Calculations.h"
+#include <iostream>
+
+int main() {
+	std::cout << "CMake Sample 2:" << std::endl;
+	std::cout << "Add value : " << add(1.2, 3.5) << std::endl;
+	std::cout << "Subtract value: " << subtract(1.2, 3.4) << std::endl;
+	std::cout << "Multiple value: " << multiple(1.2, 3.4) << std::endl;
+	std::cout << "Divide value: " << divide(1.2, 3.4) << std::endl;
+	std::cout << "calFlag value: " << calFlag << std::endl;
+	return 0;
+}
