0e7bab5777f9901c00e9393ba8ac0fca684ba86c
leichen <1327712813@qq.com>
2021-07-11 00:06:39 +0800

    Link OpenCV library manually in sample5.

---
+++ cmake_guide/sample5/sample5.cpp
@@ -0,0 +1,26 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	std::cout << "CMake Sample 5:" << std::endl;
+	if (argc != 2) {
+		std::cout << "Usage: " << argv[0] << "PathToImage" << std::endl;
+		return -1;
+	}
+	
+	cv::Mat image;
+	image = cv::imread(argv[1], cv::IMREAD_COLOR);
+
+	if (image.empty()) {
+		std::cout << "Load image failed" << std::endl;
+		return -1;
+	}
+
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Display window", image);
+
+	cv::waitKey(0);
+	return 0;
+}
